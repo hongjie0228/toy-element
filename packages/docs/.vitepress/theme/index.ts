bd778f5fe72ab187022628962c6b2950f42927db
hongjie0228 <hongjie0228@126.com>
2024-09-09 22:29:31 +0800

    feat docs

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,9 +1,11 @@
 import DefaultTheme from "vitepress/theme";
 import { type App } from "vue";
 import ToyElement from "toy-elements";
-import { ElementPlusContainer } from "@vitepress-preview/component";
+import { ElementPlusContainer } from "@vitepress-demo-preview/component";
 
-import "@vitepress-preview/component/style.css";
+
+import '@vitepress-demo-preview/component/dist/style.css'
+
 import "toy-elements/dist/index.css";
 
 export default {
---
+++ packages/docs/demo/button/Throttle.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <p>
+    <er-button @click="handleBtnClick"> with throttle</er-button>
+    <er-button :use-throttle="false" @click="handleBtnClick"
+      >without throttle</er-button
+    >
+  </p>
+</template>
